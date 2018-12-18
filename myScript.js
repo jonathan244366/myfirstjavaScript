24d49165b8e024717a9573805d8b19f729d34dc4
jonathan244366 <jonathan.l366@slcstudents.org>
2018-12-18 08:28:00 -0700

    Changed the text

---
+++ myScript.js
@@ -0,0 +1,17 @@
+//this function changed the text in the demo paragraph
+
+function myFunction () {
+    document.getElementById('demo').innerHTML="paragraph changed";
+}
+
+// Defining variables 
+
+var x = 5;
+var y = 6;
+var z = x + y;
+
+// this function changes the text in the math paragraph 
+
+function mathFunction (){
+    document.getElementById('math').innerHTML = z;
+}
